086fd33463d72b033427e1353b6a5c9da47b829b
Volodymyr Mishyn <pti.mishin@gmail.com>
2024-01-09 19:34:48 +0200

    language selector in prod mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
@@ -20,7 +20,25 @@ import { RegisterIconsService } from './services/register-icons.service';
 export class AppComponent {
   public title = $localize`SSR&i18n Angular application`;
   public icon = 'assets/img/flag_with_trident.svg';
-  constructor(private _registerIconsService: RegisterIconsService) {
+  public activeLocale: 'en-US' | 'uk' = 'en-US';
+  constructor(
+    private _registerIconsService: RegisterIconsService,
+    @Inject(LOCALE_ID) private _locale: string
+  ) {
     this._registerIconsService.registerIcons(['trident']);
+    this.activeLocale = this._locale as 'en-US' | 'uk';
+  }
+  public changeLanguage(language: string): void {
+    const pathArray = window.location.pathname.split('/');
+    const currentLanguage = pathArray[1];
+    if (currentLanguage !== language) {
+      pathArray[1] = language;
+      const newPath = pathArray.join('/');
+      window.location.href = newPath;
+    }
+  }
+
+  public isLanguageChangeAvailable(): boolean {
+    return !isDevMode();
   }
 }
